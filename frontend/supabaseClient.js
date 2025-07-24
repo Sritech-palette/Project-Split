6dad505deb233cabf96b88e4fdae5fb6943f23bc
Sritech-palette <sribitsandbytes@gmail.com>
2025-07-24 22:18:06 +0400

    Update supabaseClient.js

--- frontend/supabaseClient.js
+++ frontend/supabaseClient.js
@@ -2,3 +2,5 @@ import { createClient } from '@supabase/supabase-js'
 //Replace these w/ thy own values from Supabase Dashboard > Settings > API
 const supabaseUrl = 'https://Project Split.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml6eXlrb3dmaGNycWx1aWlwdnNtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI2MDYxMjYsImV4cCI6MjA2ODE4MjEyNn0.noHFdo1sUyBTarrPCROJTT0BD0z6gMapWpDN8NmQveU'
+
+export const supabase = createClient(supabaseUrl, supabaseKey)
